5a74306a1aaf2cedbc281b35972f9a8ad3ca1c90
DeveloperZER0 <w-piwowar@protonmail.com>
2026-03-27 15:58:03 +0100

    Kuchnia, Salon, Strych

--- Assets/Scripts/Scene/SceneSwitcher.cs
+++ Assets/Scripts/Scene/SceneSwitcher.cs
@@ -1,23 +1,97 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
 public class SceneSwitcher : MonoBehaviour
 {
+  [System.Serializable]
+  private class PseudoSceneEntry
+  {
+    public string sceneId;
+    public GameObject rootObject;
+  }
+
     [SerializeField] private Image fadePanel;
     [SerializeField] private float fadeDuration = 1f;
+  [SerializeField] private PseudoSceneEntry[] pseudoScenes;
+  [SerializeField] private int initialSceneIndex;
+
+  private bool isSwitching;
+  private int currentSceneIndex = -1;
 
     void Start() {
-      StartCoroutine(FadeIn());
+    SwitchToIndexImmediate(initialSceneIndex);
+    StartCoroutine(FadeIn());
     }
 
     public void FadeToScene(string sceneName) {
-        StartCoroutine(FadeOut(sceneName));
+    FadeToPseudoScene(sceneName);
     }
 
-    IEnumerator FadeOut(string sceneName) {
+  public void FadeToPseudoScene(string sceneId)
+  {
+    int targetIndex = FindSceneIndex(sceneId);
+
+    if (targetIndex < 0)
+    {
+      Debug.LogWarning($"SceneSwitcher: pseudo-scene '{sceneId}' not found.");
+      return;
+    }
+
+    FadeToIndex(targetIndex);
+  }
+
+  public void FadeToIndex(int targetIndex)
+  {
+    if (isSwitching)
+    {
+      return;
+    }
+
+    if (!IsIndexValid(targetIndex))
+    {
+      Debug.LogWarning($"SceneSwitcher: invalid pseudo-scene index {targetIndex}.");
+      return;
+    }
+
+    StartCoroutine(FadeSwitch(targetIndex));
+  }
+
+  public void SwitchToIndexImmediate(int targetIndex)
+  {
+    if (!IsIndexValid(targetIndex))
+    {
+      return;
+    }
+
+    SetActivePseudoScene(targetIndex);
+  }
+
+  private IEnumerator FadeSwitch(int targetIndex)
+  {
+    isSwitching = true;
+
+    yield return FadeOut();
+    SetActivePseudoScene(targetIndex);
+    yield return FadeIn();
+
+    isSwitching = false;
+  }
+
+  private IEnumerator FadeOut() {
+
+    if (fadePanel == null)
+    {
+      yield break;
+    }
+
+    if (fadeDuration <= 0f)
+    {
+      Color instantColor = fadePanel.color;
+      instantColor.a = 1f;
+      fadePanel.color = instantColor;
+      yield break;
+    }
 
       float t = 0f;
         Color color = fadePanel.color;
@@ -29,10 +103,24 @@ public class SceneSwitcher : MonoBehaviour
             yield return null;
         }
 
-        SceneManager.LoadScene(sceneName);
+    color.a = 1f;
+    fadePanel.color = color;
     }
 
-    IEnumerator FadeIn() {
+  private IEnumerator FadeIn() {
+
+    if (fadePanel == null)
+    {
+      yield break;
+    }
+
+    if (fadeDuration <= 0f)
+    {
+      Color instantColor = fadePanel.color;
+      instantColor.a = 0f;
+      fadePanel.color = instantColor;
+      yield break;
+    }
 
       float t = 0f;
         Color color = fadePanel.color;
@@ -44,8 +132,44 @@ public class SceneSwitcher : MonoBehaviour
             yield return null;
         }
 
+          color.a = 0f;
+          fadePanel.color = color;
+
+        }
+
+        private void SetActivePseudoScene(int targetIndex)
+        {
+          for (int i = 0; i < pseudoScenes.Length; i++)
+          {
+            if (pseudoScenes[i].rootObject != null)
+            {
+              pseudoScenes[i].rootObject.SetActive(i == targetIndex);
+            }
+          }
+
+          currentSceneIndex = targetIndex;
+        }
+
+        private int FindSceneIndex(string sceneId)
+        {
+          if (string.IsNullOrEmpty(sceneId))
+          {
+            return -1;
+          }
+
+          for (int i = 0; i < pseudoScenes.Length; i++)
+          {
+            if (pseudoScenes[i].sceneId == sceneId)
+            {
+              return i;
+            }
+          }
+
+          return -1;
+        }
+
+        private bool IsIndexValid(int index)
+        {
+          return index >= 0 && index < pseudoScenes.Length;
     }
-
-
-
 }
